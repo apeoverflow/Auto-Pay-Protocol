eac1a1bbcd3d26bbc761589e069d70f8a63d9ada
Kieran Williams <kieranmarcus@gmail.com>
2026-02-05 21:48:58 +0000

    fix exceeding max event listners issue

--- relayer/src/utils/logger.ts
+++ relayer/src/utils/logger.ts
@@ -1,21 +1,22 @@
 import pino from 'pino'
 
-export function createLogger(name: string, level = 'info') {
-  return pino({
-    name,
-    level,
-    transport:
-      process.env.NODE_ENV !== 'production'
-        ? {
-            target: 'pino-pretty',
-            options: {
-              colorize: true,
-              translateTime: 'HH:MM:ss',
-              ignore: 'pid,hostname',
-            },
-          }
-        : undefined,
-  })
+const rootLogger = pino({
+  level: process.env.LOG_LEVEL || 'info',
+  transport:
+    process.env.NODE_ENV !== 'production'
+      ? {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'HH:MM:ss',
+            ignore: 'pid,hostname',
+          },
+        }
+      : undefined,
+})
+
+export function createLogger(name: string) {
+  return rootLogger.child({ name })
 }
 
-export const logger = createLogger('relayer')
+export const logger = rootLogger
